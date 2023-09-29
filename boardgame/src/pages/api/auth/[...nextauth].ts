a878c86f0660c9112323f507fb262f38c21e59d0
Robert <robert.mihai.software@gmail.com>
2023-09-29 17:36:57 +0200

    rebuilt fe

--- boardgame/src/pages/api/auth/[...nextauth].ts
+++
@@ -1,5 +0,0 @@
-import NextAuth from "next-auth";
-
-import { authOptions } from "boardgame/server/auth";
-
-export default NextAuth(authOptions);
---
+++ web/src/pages/asd/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeASD() {
+  return <div>HomeASD</div>;
+}
+
+export default HomeASD;
