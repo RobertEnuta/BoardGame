f55026191f1018049dc88d27220410ee6677edef
Robert Enuță <103112642+RobertEnuta@users.noreply.github.com>
2023-11-05 22:19:21 +0100

    Vercel analytics  (#9)
    * Base FE app created
    
    Created base files for the project and a FE app
    
    * rebuilt fe
    
    * refreshed project and added basic classes
    
    * some changes made to the timeline, added the textbox, created sidemenu nav, all need to be worked on more
    
    * added prisma to the project and started creating backend
    
    * added platescale db
    
    * playwright package error fixed
    
    * fixed svg error
    
    * build ready
    
    * implementing vercel analytics

--- web/src/components/Typebar.tsx
+++ web/src/components/Typebar.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Typebar() {
   return (
-    <div className="sticky top-2 flex flex-row items-center justify-center">
+    <div className="sticky top-2 flex flex-row items-center justify-center pb-2">
       <img
         src="/user.png"
-        className="m-2 h-12 w-12 rounded-full border bg-white"
+        className="mr-2 h-12 w-12 rounded-full border bg-white"
       />
       <div className="  my-2 grid grow grid-cols-2 grid-rows-1 items-center rounded-xl border border-t-0 bg-white px-2 py-4 text-slate-900 ">
         Start typing here
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
-import "~/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
+import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -11,6 +12,7 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
+      <Analytics />
       <Component {...pageProps} />
     </main>
   );
